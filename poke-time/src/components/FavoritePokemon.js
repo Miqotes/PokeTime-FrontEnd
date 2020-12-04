449b699c8638994f42f1592e73f45c77c1e6e643
Miqotes <ashleelennab@gmail.com>
2020-12-04 12:10:24 -0800

    Removed unnecessary DnD file

--- poke-time/src/components/FavoritePokemon.js
+++ poke-time/src/components/FavoritePokemon.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import DraggablePokemon from './DraggablePokemon'
 
 export default function FavoritePokemon(){
 
     return(
         <div>
-            
+            <h2>Favorites</h2>
         </div>
     )
 }
